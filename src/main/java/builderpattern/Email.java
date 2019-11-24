030a27e26e8bd94ff36ca6e86baff13bdfe2945a
Fatih Kemal Akcay <fatihkemalakcay@gmail.com>
2019-11-24 00:37:55 +0000

    first

---
+++ src/main/java/builderpattern/Email.java
@@ -0,0 +1,71 @@
+package builderpattern;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Email {
+
+    private final String title;
+    private final String recipients;
+    private final String message;
+
+    public Email(String title, String recipients, String message) {
+        this.title = title;
+        this.recipients = recipients;
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRecipients() {
+        return recipients;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public static class EmailBuilder {
+
+        private Set recipients = new HashSet();
+        private String title;
+        private String greeting;
+        private String mainText;
+
+        public EmailBuilder addRecipient(String recipient) {
+            this.recipients.add(recipient);
+            return this;
+        }
+
+        public EmailBuilder removeRecipient(String recipient) {
+            this.recipients.remove(recipient);
+            return this;
+        }
+
+        public EmailBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public EmailBuilder setGreeting(String greeting) {
+            this.greeting = greeting;
+            return this;
+        }
+
+        public EmailBuilder setMainText(String mainText) {
+            this.mainText = mainText;
+            return this;
+        }
+
+        public Email build() {
+            String message = greeting + "\n" + mainText + "\n";
+            return new Email(title, recipients.toString(), message);
+        }
+
+    }
+}
+
+
